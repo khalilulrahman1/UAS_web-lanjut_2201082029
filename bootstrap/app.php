2f5306f1f4294cb3bcbc57e8249fa4e09628e2e7
khalilulrahman1 <khalil29s03@gmail.com>
2024-01-15 09:10:32 +0700

    first commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -43,10 +43,10 @@ $app->singleton(
 
 /*
 |--------------------------------------------------------------------------
-| Return The Application
+| Return The application
 |--------------------------------------------------------------------------
 |
-| This script returns the application instance. The instance is given to
+| This script eturns the application instance. The instance is given to
 | the calling script so we can separate the building of the instances
 | from the actual running of the application and sending responses.
 |
